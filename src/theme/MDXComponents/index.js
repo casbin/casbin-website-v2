894d381a22dfcaa37a55ca5d4efadd905b56690a
YunShu <im@yunshu.site>
2022-07-07 00:29:27 +0800

    feat: Syncing tab choices (#48)

---
+++ src/theme/MDXComponents/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MDXComponents from '@theme-original/MDXComponents';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+
+export default {
+  ...MDXComponents,
+  Tabs: Tabs,
+  TabItem: TabItem,
+};
