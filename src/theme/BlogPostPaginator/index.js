6ebc1045f2fb81d5e08530ee507d26104af457df
YunShu <im@yunshu.site>
2022-08-30 20:09:31 +0800

    fix: Remove disqus ads

--- src/theme/BlogPostPaginator/index.js
+++ src/theme/BlogPostPaginator/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import BlogPostPaginator from '@theme-original/BlogPostPaginator';
 import { DiscussionEmbed } from 'disqus-react';
+import Head from '@docusaurus/Head';
 
 export default function BlogPostPaginatorWrapper(props) {
   return (
     <>
+      <Head
+        style={[{
+          "cssText": `
+        iframe[sandbox]:not([sandbox=""]) {
+          display: none !important;
+      }
+      `
+        }]} />
       <BlogPostPaginator {...props} />
       <DiscussionEmbed
         shortname="casbin"
--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
 import { DiscussionEmbed } from 'disqus-react';
+import Head from '@docusaurus/Head';
 
 export default function DocPaginatorWrapper(props) {
   return (
     <>
+      <Head
+        style={[{
+          "cssText": `
+        iframe[sandbox]:not([sandbox=""]) {
+          display: none !important;
+      }
+      `
+        }]} />
       <DocPaginator {...props} />
       <DiscussionEmbed
         shortname="casbin"
