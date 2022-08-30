ce4df285d39a3880ad86907fc90f0aabc3d08a53
YunShu <im@yunshu.site>
2022-08-31 01:59:28 +0800

    refactor: Switch to Giscus commenting system

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import { DiscussionEmbed } from 'disqus-react';
+import Giscus from '@giscus/react';
+
 function Editor() {
   return (
     <Layout title="Editor" description="Casbin Online Editor">
       <div>
         <iframe
           src="https://casbin.org/casbin-editor/"
-          style={{ width: '100%', height: '100vh', display: 'block' }}
+          style={{ width: '100%', height: '800px', display: 'block' }}
           title="Casbin-editor"
-          allowFullScreen={true}
+          scrolling="no"
         />
       </div>
-      <DiscussionEmbed
-        shortname="casbin"
-        config={
-          {
-            language: 'en',
-          }
-        } />
+      <br />
+      <div style={{ width: "60vw", margin: "0 auto" }}>
+        <Giscus
+          id="comments"
+          repo="casbin/casbin"
+          repoId="MDEwOlJlcG9zaXRvcnk4NzYxNzUwOA=="
+          category="Docs comments"
+          categoryId="DIC_kwDOBTjv5M4CRIiA"
+          mapping="pathname"
+          strict="0"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="top"
+          theme="light"
+          lang="en"
+          loading="lazy"
+        />
+      </div>
     </Layout>
   );
 }
--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
-import { DiscussionEmbed } from 'disqus-react';
+import Giscus from '@giscus/react';
+import { useColorMode } from '@docusaurus/theme-common';
 
 export default function DocPaginatorWrapper(props) {
   return (
     <>
       <DocPaginator {...props} />
-      <DiscussionEmbed
-        shortname="casbin"
-        config={
-          {
-            language: 'en',
-          }
-        } />
+      <br />
+      <Giscus
+        id="comments"
+        repo="casbin/casbin"
+        repoId="MDEwOlJlcG9zaXRvcnk4NzYxNzUwOA=="
+        category="Docs comments"
+        categoryId="DIC_kwDOBTjv5M4CRIiA"
+        mapping="pathname"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme={useColorMode().colorMode}
+        lang="en"
+        loading="lazy"
+      />
     </>
   );
 }
