24a4fb251a3f2a7539b785774ab802c6e444a48b
YunShu <im@yunshu.site>
2022-08-31 07:05:19 +0800

    refactor: Switch to Giscus commenting system (#86)

--- src/theme/BlogPostPaginator/index.js
+++ src/theme/BlogPostPaginator/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import BlogPostPaginator from '@theme-original/BlogPostPaginator';
-import { DiscussionEmbed } from 'disqus-react';
+import Giscus from '@giscus/react';
+import { useColorMode } from '@docusaurus/theme-common';
 
 export default function BlogPostPaginatorWrapper(props) {
   return (
     <>
       <BlogPostPaginator {...props} />
-      <DiscussionEmbed
-        shortname="casbin"
-        config={
-          {
-            language: 'en',
-          }
-        } />
+      <br />
+      <Giscus
+        id="comments"
+        repo="casbin/casbin"
+        repoId="MDEwOlJlcG9zaXRvcnk4NzYxNzUwOA=="
+        category="Docs comments"
+        categoryId="DIC_kwDOBTjv5M4CRIiA"
+        mapping="pathname"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme={useColorMode().colorMode}
+        lang="en"
+        loading="lazy"
+      />
     </>
   );
 }
