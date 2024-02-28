b37bad089da271d02d3963db232ebf6f92f91836
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-02-28 15:40:18 +0800

    fix: editor-iframe-height to low hidden button (#235)

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -8,7 +8,7 @@ function Editor() {
       <div>
         <iframe
           src="https://editor.casbin.org/"
-          style={{width: "100%", height: "800px", display: "block"}}
+          style={{width: "100%", height: "850px", display: "block"}}
           title="Casbin-editor"
           scrolling="no"
         />
