d563835b9f37edbafeee65c133fdbd266b90c4bb
Coki <hashcookie404@gmail.com>
2024-07-28 18:55:05 +0800

    feat: update locale list in docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -318,6 +318,6 @@ var _hmt = _hmt || [];
   ],
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "zh", "zh-Hant", "ko", "ru", "fr", "de", "ja", "es", "pt", "uk", "th", "ar", "id", "vi", "it", "ms", "tr"],
+    locales: ["en", "zh", "zh-TW", "ko", "ru", "fr", "de", "ja", "es", "pt", "uk", "th", "ar", "id", "vi", "it", "ms", "tr"],
   },
 };
