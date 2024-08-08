0475ace46a9e9204f3d26333e3c6e9d996764813
Coki <hashcookie404@gmail.com>
2024-08-08 21:53:55 +0800

    refactor: update casibase.js initialization and remove external script inclusion

--- static/js/casibase.js
+++ static/js/casibase.js
@@ -1,5 +1,16 @@
-/* eslint-disable no-undef */
-initCasibaseChat({
-  endpoint: "https://ai.casbin.com",
-  themeColor: "rgb(64,59,121)",
-});
+(function(w, d, s, c, i) {
+  const j = d.createElement(s);
+  j.async = false;
+  j.src = "https://tcdn.casibase.org/casibase.js";
+  j.onload = function() {
+    w[c]("init", {
+      endpoint: "https://ai.casbin.com",
+      themeColor: "rgb(64,59,121)",
+    });
+  };
+  const f = d.getElementsByTagName(s)[0];
+  f.parentNode.insertBefore(j, f);
+  w[c] = w[c] || function() {
+    (w[c].q = w[c].q || []).push(arguments);
+  };
+})(window, document, "script", "casibaseChat");
