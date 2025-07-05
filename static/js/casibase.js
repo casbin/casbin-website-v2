ab0ad4b82a9f79d2b25b7d03ccfaf9435702e8ce
@mr-liu-who-loves-tapping-code <2256619513@qq.com>
2025-07-05 21:57:56 +0800

    fix:optimize

--- static/js/casibase.js
+++ static/js/casibase.js
@@ -1,27 +1,25 @@
 (function(w, d, s, c, i) {
-  const script = d.createElement(s);
-  script.async = false;
-  script.src = "https://tcdn.casibase.org/casibase.js";
+  const j = d.createElement(s);
+  j.async = false;
+  j.src = "https://tcdn.casibase.org/casibase.js";
 
-  // Determine whether to show the popup: first visit or more than 24 hours since last visit
+  
   function shouldShowPopup() {
     const lastVisitTime = localStorage.getItem("casibase_visited");
-    
-    // If visited before, check if within 24 hours
+   
     if (lastVisitTime && Date.now() - lastVisitTime < 24 * 60 * 60 * 1000) {
       return false;
     }
     
-    // First visit or more than 24 hours have passed, update visit time and show popup
     localStorage.setItem("casibase_visited", Date.now());
     return true;
   }
 
-  script.onload = function() {
-    // Reverse conditional check to reduce indentation
+  j.onload = function() {
+   
     if (!shouldShowPopup()) return;
 
-    // Initialize popup component
+   
     w[c]("init", {
       endpoint: "https://ai.casbin.com",
       themeColor: "rgb(64,59,121)",
@@ -30,12 +28,11 @@
     });
   };
 
-  // Insert script into document
-  const f = d.getElementsByTagName(s)[0];
-  f.parentNode.insertBefore(script, f);
   
-  // Initialize global function
+  const f = d.getElementsByTagName(s)[0];
+  f.parentNode.insertBefore(j, f);
+  
   w[c] = w[c] || function() {
-    (w[c].q = w[c].q || [])?.push(arguments);
+    (w[c].q = w[c].q || []).push(arguments);
   };
 })(window, document, "script", "casibaseChat");
