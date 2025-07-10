ca867c1e877089da3464b7597d8ced8fe8ba2899
@mr-liu-who-loves-tapping-code <2256619513@qq.com>
2025-07-10 21:25:34 +0800

    fix:optimize18

--- static/js/casibase.js
+++ static/js/casibase.js
@@ -1,49 +1,30 @@
 (function (window, document, scriptTag, casibaseKey) {
-  'use strict'; 
-
-
+  'use strict';
   const script = document.createElement(scriptTag);
   script.async = false;
   script.src = 'https://tcdn.casibase.org/casibase.js';
 
-  
   function shouldShowPopup() {
-    const lastVisitTime = localStorage.getItem('casibase_visited');
-    if (lastVisitTime) {
-      const timeDiff = Date.now() - Number(lastVisitTime); 
-      if (timeDiff < 24 * 60 * 60 * 1000) {
-        return false;
-      }
-    }
-    localStorage.setItem('casibase_visited', Date.now().toString());
+    const lastVisit = localStorage.getItem('casibase_visited');
+    if (lastVisit && Date.now() - Number(lastVisit) < 86400000) return false;
+    localStorage.setItem('casibase_visited', Date.now());
     return true;
   }
 
-
   script.onload = function () {
-    if (!shouldShowPopup()) return; 
-
+    if (!shouldShowPopup()) return;
     window[casibaseKey]('init', {
       endpoint: 'https://ai.casbin.com',
-      themeColor: 'rgb(64, 59, 121)', 
+      themeColor: 'rgb(64, 59, 121)',
       popupTime: 5,
-      onClose: () => {
-        localStorage.setItem('casibase_visited', Date.now().toString()); 
-      }
+      onClose: () => localStorage.setItem('casibase_visited', Date.now())
     });
   };
 
-
   const firstScript = document.getElementsByTagName(scriptTag)[0];
-  firstScript?.parentNode?.insertBefore(script, firstScript); 
+  firstScript?.parentNode?.insertBefore(script, firstScript);
 
-  
-  if (typeof window[casibaseKey] !== 'function') {
-    window[casibaseKey] = function (...args) { 
-      (window[casibaseKey].q = window[casibaseKey].q || []).push(args);
-    };
-  } else {
-
-    window[casibaseKey].q = window[casibaseKey].q || [];
-  }
+  window[casibaseKey] = window[casibaseKey] || function (...args) {
+    (window[casibaseKey].q = window[casibaseKey].q || []).push(args);
+  };
 })(window, document, 'script', 'casibaseChat');
