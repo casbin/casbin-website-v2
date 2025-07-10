29fc3c6d29f83c85b93f7062893730ca3d416b15
@mr-liu-who-loves-tapping-code <2256619513@qq.com>
2025-07-10 21:15:06 +0800

    fix:optimize15

--- static/js/casibase.js
+++ static/js/casibase.js
@@ -1,35 +1,34 @@
-(function(w, d, s, c, i) {
-  const j = d.createElement(s);
-  j.async = false;
-  j.src = "https://tcdn.casibase.org/casibase.js";
+(function(w, d, s, c) { 
+  const script = d.createElement(s); 
+  script.async = false;
+  script.src = "https://tcdn.casibase.org/casibase.js";
 
-  
+
   function shouldShowPopup() {
     const lastVisitTime = localStorage.getItem("casibase_visited");
-   
     if (lastVisitTime && Date.now() - lastVisitTime < 24 * 60 * 60 * 1000) {
       return false;
     }
-    
     localStorage.setItem("casibase_visited", Date.now());
     return true;
   }
 
-  j.onload = function() {
-   
-    if (!shouldShowPopup()) return;
+  script.onload = function() {
+    if (!shouldShowPopup()) return; 
 
-   
     w[c]("init", {
       endpoint: "https://ai.casbin.com",
       themeColor: "rgb(64,59,121)",
       popupTime: 5,
-      onClose: () => localStorage.setItem("casibase_visited", Date.now())
+      onClose: () => localStorage.setItem("casibase_visited", Date.now()) 
     });
   };
-  const f = d.getElementsByTagName(s)[0];
-  f.parentNode.insertBefore(j, f);
+
+  const firstScript = d.getElementsByTagName(s)[0]; 
+  firstScript.parentNode.insertBefore(script, firstScript);
+
+
   w[c] = w[c] || function() {
     (w[c].q = w[c].q || []).push(arguments);
   };
-})(window, document, "script", "casibaseChat");
+})(window, document, "script", "casibaseChat"); 
