cbddfe44fd91118dd6f253b970bdcf176fa12f31
Yang Luo <hsluoyz@gmail.com>
2025-12-23 01:00:03 +0800

    feat: remove Open Collective section from landing page (#382)

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
 import {UserList} from "@site/src/components/UserList";
 import Translate from "@docusaurus/Translate";
-import {useWindowSize} from "@docusaurus/theme-common";
 import EditorPreview from "../components/EditorPreview";
 import LanguageIntegration from "../components/LanguageIntegration";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
@@ -155,18 +154,6 @@ function Showcase() {
   );
 }
 
-function OpenCollective() {
-  if (useWindowSize() === "mobile") {
-    return (
-      <iframe title="Sponsors" src="https://opencollective.com/casbin/banner.html" style={{width: "100%", height: "1100px", display: "block"}}></iframe>
-    );
-  } else {
-    return (
-      <iframe title="Sponsors" src="https://opencollective.com/casbin/banner.html" style={{width: "100%", height: "650px", display: "block"}}></iframe>
-    );
-  }
-}
-
 export default function Home() {
   return (
     <Layout
@@ -181,7 +168,6 @@ export default function Home() {
         <PolicyEnforcement />
         <RoleManager />
         <Showcase />
-        <OpenCollective />
       </main>
     </Layout>
   );
