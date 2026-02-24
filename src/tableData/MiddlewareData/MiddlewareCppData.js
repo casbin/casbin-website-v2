73bae9e99f7ebe362735dfeaa62b38cb051057f9
Gustavo Bertoli <gubertoli@gmail.com>
2026-02-24 14:29:55 +0100

    feat: Add mention to Mosquitto MQTT broker v2.0+ plugin (#448)

--- src/tableData/MiddlewareData/MiddlewareCppData.js
+++ src/tableData/MiddlewareData/MiddlewareCppData.js
@@ -5,6 +5,13 @@ export const MiddlewareCppData = [
       "A HTTP and reverse proxy server, a mail proxy server, and a generic TCP/UDP proxy server, via plugin: [nginx-casbin-module](https://github.com/casbin-cpp/nginx-casbin-module)",
     image: "/img/ecosystem/nginx.png",
   },
+  {
+    title: "[Mosquitto Plugin](https://github.com/gubertoli/mosquitto-casbin)",
+    author: "[@gubertoli](https://github.com/gubertoli)",
+    description:
+      "Plugin for Mosquitto MQTT broker v2.0+ to manage access control via Casbin",
+    image: "/img/ecosystem/mosquitto.png",
+  },
 ].map((item) => {
   return {
     ...item,
